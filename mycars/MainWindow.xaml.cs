f3fb7a74da33c01ee95128b34080cedac8bdb97b
voidAsterisk <drif7walk@gmail.com>
2018-03-14 18:41:02 -0800

    Commit

---
+++ mycars/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+namespace mycars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Parbaude vai eksiste darba fails
+            // ja eksiste tad nolasit failu un pievienot watcher
+            if (File.Exists("cars.csv"))
+            {
+                UpdateList(null, null);
+
+                // Sis kods monetore failu vai taja nerodas izmanias un tad parveido sarakstu
+                FileSystemWatcher fwatcher = new FileSystemWatcher();
+                fwatcher.Path = ".";
+                fwatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
+                fwatcher.Changed += new FileSystemEventHandler(UpdateList);
+                fwatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Open the add car menu
+            addCarsWindow ac = new addCarsWindow();
+            ac.Owner = this;
+            ac.Show();
+        }
+
+        // Nonemt izveleto
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (carListBox.SelectedIndex != -1)
+            {
+                string tempFile = System.IO.Path.GetTempFileName();
+
+                using (var sr = new StreamReader("cars.csv"))
+                using (var sw = new StreamWriter(tempFile))
+                {
+                    string line;
+                    int selectedIndex = 0;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (selectedIndex != carListBox.SelectedIndex)
+                            sw.WriteLine(line);
+                        selectedIndex++;
+                    }
+                }
+                
+                File.Delete("cars.csv");
+                File.Move(tempFile, "cars.csv");
+                carListBox.Items.RemoveAt(carListBox.SelectedIndex);
+
+
+            }
+        }
+
+        private void UpdateList(object sender, FileSystemEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader("cars.csv");
+                carListBox.Items.Clear();
+                String line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    String[] strList = line.Split(',');
+                    String myStr = "No " + strList[0] + " Lidz " + strList[1] + " " + strList[2] + " " + strList[3];
+                    carListBox.Items.Add(myStr);
+                }
+                file.Close();
+            });
+        }
+        
+        
+    }
+}
---
+++ mycars/addCarsWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.IO;
+
+namespace mycars
+{
+    /// <summary>
+    /// Interaction logic for addCarsWindow.xaml
+    /// </summary>
+    public partial class addCarsWindow : Window
+    {
+        public addCarsWindow()
+        {
+            InitializeComponent();
+
+            /* 0 - 24 lai uzradas */
+            for (int i = 1; i < 25; i++)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                comboBox.Items.Add(i);
+            }
+            for (int i = 1; i < 25; i++)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                comboBox_Copy.Items.Add(i);
+            }
+        }
+
+        // Atcelt poga
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Pievienot poga
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bool NoErrors = true;
+            // Parbaudit vai visas vertibas ir ievaditas.
+            if (comboBox.SelectedIndex == -1)
+            {
+                comboBox.Focus();
+                NoErrors = false;
+            }
+            if (comboBox_Copy.SelectedIndex == -1)
+            {
+                comboBox_Copy.Focus();
+                NoErrors = false;
+            }
+            if (nameBox.Text == "")
+            {
+                nameBox.Focus();
+                NoErrors = false;
+            }
+            if (carNumberBox.Text == "")
+            {
+                carNumberBox.Focus();
+                NoErrors = false;
+            }
+
+            /*
+            int from = (int)comboBox.SelectedItem;
+            int to = (int)comboBox.SelectedItem;
+            */
+
+            if (NoErrors)
+            {
+                // Ierakstit datus faila
+                using (StreamWriter w = File.AppendText("cars.csv"))
+                {
+                    // Prveidot ievaditas vertibas CSV faila formata
+                    String fout = "";
+                    fout += comboBox.SelectedItem.ToString() + ",";
+                    fout += comboBox_Copy.SelectedItem.ToString() + ",";
+                    fout += nameBox.Text + ",";
+                    fout += carNumberBox.Text + "\r\n";
+
+
+                    w.Write(fout);
+                }
+
+                // Aizvert logu
+                this.Close();
+            }
+        }
+    }
+}
